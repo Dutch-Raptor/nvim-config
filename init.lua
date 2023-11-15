383dadebe1d1c37a7b505ecff4538c22ed975ae7
Pieter Huizenga <mail@pieterhuizenga.com>
2023-11-15 12:01:30 +0100

    Add open file on last position

--- init.lua
+++ init.lua
@@ -237,6 +237,26 @@ vim.api.nvim_create_autocmd('TextYankPost', {
 })
 
 
+-- From vim defaults.vim
+-- ---
+-- When editing a file, always jump to the last known cursor position.
+-- Don't do it when the position is invalid, when inside an event handler
+-- (happens when dropping a file on gvim) and for a commit message (it's
+-- likely a different one than last time).
+local last_position_group = vim.api.nvim_create_augroup('LastPosition', { clear = true })
+vim.api.nvim_create_autocmd('BufReadPost', {
+  group = last_position_group,
+  callback = function(args)
+    local valid_line = vim.fn.line([['"]]) >= 1 and vim.fn.line([['"]]) < vim.fn.line('$')
+    local not_commit = vim.b[args.buf].filetype ~= 'commit'
+
+    if valid_line and not_commit then
+      vim.cmd([[normal! g`"]])
+    end
+  end,
+})
+
+
 
 -- [[ Configure Treesitter ]]
 -- See `:help nvim-treesitter`
